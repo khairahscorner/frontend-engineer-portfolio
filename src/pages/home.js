713d18eace3be9f627f5c54d55deeea34357b911
Airah Yusuff <airahyusuff@gmail.com>
2023-08-27 07:52:07 +0000

    updated

--- src/pages/home.js
+++ src/pages/home.js
@@ -22,146 +22,16 @@ import {ReactComponent as IGIcon} from '../assets/instagram.svg';
 import {ReactComponent as QuoraIcon} from '../assets/quora.svg';
 import {ReactComponent as PinterestIcon} from '../assets/pinterest.svg';
 import {ReactComponent as PickyIcon} from '../assets/picky.svg';
-import HTMLIcon from "../assets/html.svg";
-import SassIcon from "../assets/sass.svg";
-import TWIcon from "../assets/tailwindcss.svg";
-import StyledIcon from "../assets/styled.png";
-import ReactIcon from "../assets/react.svg";
-import GatsbyIcon from "../assets/gatsby.svg";
-import NuxtIcon from "../assets/nuxt.svg";
-import VueIcon from "../assets/vue.svg";
-import GraphqlIcon from "../assets/graphql.svg";
-import SqlIcon from "../assets/sql.svg";
-import VscodeIcon from "../assets/vscode.svg";
-import GitIcon from "../assets/git.svg";
+import {tools, projects, settings} from "../components/resources";
 import resume from '../assets/airahyusuff.pdf';
 
 export const findImg = (val, arr) => {
   const result = arr.find((el) => el.public_id.includes(val) === true)
   return result.public_id
 }
-const tools = [
-  {
-    url: "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics",
-    logo: HTMLIcon
-  },
-  {
-    url: "https://sass-lang.com/",
-    logo: SassIcon
-  },
-  {
-    url: "https://tailwindcss.com/",
-    logo: TWIcon
-  },
-  {
-    url: "https://styled-components.com/",
-    logo: StyledIcon
-  },
-  {
-    url: "https://reactjs.org/",
-    logo: ReactIcon
-  },
-  {
-    url: "https://www.gatsbyjs.com/",
-    logo: GatsbyIcon
-  },
-  {
-    url: "https://nuxtjs.org/",
-    logo: NuxtIcon
-  },
-  {
-    url: "https://vuejs.org/",
-    logo: VueIcon
-  },
-  {
-    url: "https://graphql.org/",
-    logo: GraphqlIcon
-  },
-  {
-    url: "https://www.postgresql.org/",
-    logo: SqlIcon
-  },
-  {
-    url: "https://code.visualstudio.com/",
-    logo: VscodeIcon
-  },
-  {
-    url: "https://git-scm.com/",
-    logo: GitIcon
-  },
-]
-const projects = [
-  {
-    name: "Callerview Dashboard",
-    url: "https://callerview.netlify.app",
-    mark: "callerview"
-  },
-  {
-    name: "AutiMS",
-    url: "https://autims.netlify.app",
-    mark: "autims"
-  },
-  {
-    name: "Teume project",
-    url: "https://treasureproject.netlify.app",
-    mark: "treasure"
-  },
-  {
-    name: "MCF",
-    url: "https://mentalcare.life",
-    mark: "mcf"
-  },
-  {
-    name: "Fitila",
-    url: "https://fitila.mentalcare.life",
-    mark: "fitila"
-  },
-  {
-    name: "Filter Pay",
-    url: "https://filter-pay.netlify.app",
-    mark: "filter"
-  },
-  {
-    name: "GitHub Repos",
-    url: "https://githubgraphql.netlify.app/",
-    mark: "graphql"
-  },
-  {
-    name: "MePaint",
-    url: "https://mepaint.netlify.app",
-    mark: "meyer"
-  },
-  {
-    name: "This Portfolio",
-    url: "#",
-    mark: "portfolio"
-  },
-]
 
 const Home = () => {
   const [currentYear, setCurrentYear] = useState("");
-  const settings = {
-    slidesToShow: 6,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    variableWidth: true,
-    infinite: true,
-    arrows: true,
-    dots: false,
-    pauseOnHover: true,
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 4
-      }
-    }, {
-      breakpoint: 520,
-      settings: {
-        slidesToShow: 2
-      }
-    }]
-  };
   const [allImages, setAllImages] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -200,9 +70,9 @@ const Home = () => {
       <MainWrapper>
         <div className="first-section inner-content">
           <div className="intro">
-            <h1 className="intro-heading">Hi, I'm Airah Yusuff.</h1>
+            <h1 className="intro-heading">Hi, I'm Airat Yusuff.</h1>
             <p className="intro-desc">
-              Frontend Engineer &#x2022; Technical Writer
+              Software Engineer &#x2022; AWS Community Builder &#x2022; Cloud Services Enthusiast
             </p>
             <div className="separator"></div>
             <div className="social-icons">
@@ -260,15 +130,12 @@ const Home = () => {
               <h2 className="section-heading">Brief Intro.</h2>
               <div className="bio">
                 <p>
-                  I'm a Frontend Engineer with 3+ years of experience developing
-                  visually appealing web applications with delightful user
+                  I'm a software engineer with about 3 years of experience developing
+                  customer-focused solutions with a delightful user
                   experience.{" "}
                 </p>
                 <p>
-                  As an avid learner, I thrive in work environments that provide
-                  measurable metrics for career growth, great opportunities for
-                  skills improvement and promotes close team collaborations and
-                  soft skills acquisition.
+                  As an avid learner, I thrive in work environments that provide opportunities for career growth and embrace diversity potentials.
                 </p>
               </div>
             </div>
@@ -321,16 +188,36 @@ const Home = () => {
               <h2 className="section-heading">Outside of Work.</h2>
               <div className="bio mb-20">
                 <p>
-                I create modest fashion, lifestyle and skincare content for my blog and different social media platforms!🥰{" "}
+                I contribute to the AWS Community Builders blog as a Community Builder, and I am also a member of tech communities like She Code Africa, Tech Sisters, etc.
                 </p>
-                <p>I also share general helpful tips, blogging resources and technical articles.</p>
+                <p>I also create technical content for my blog and share general helpful tips.
+                </p>
               </div>
               <div className="social-icons">
               <div className="round-icon">
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://www.khairahscorner.com"
+                  href="https://aws.amazon.com/developer/community/community-builders/community-builders-directory"
+                >
+                  <WebIcon />
+                </a>
+              </div>
+
+              <div className="round-icon">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://khairahscorner.hashnode.dev"
+                >
+                  <HashnodeIcon />
+                </a>
+              </div>
+              <div className="round-icon">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.tech-sisters.com/"
                 >
                   <WebIcon />
                 </a>
@@ -339,18 +226,18 @@ const Home = () => {
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://youtube.com/khairahscorner"
+                  href="https://instagram.com/airahinthecloud"
                 >
-                  <YTIcon />
+                  <IGIcon />
                 </a>
               </div>
               <div className="round-icon">
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://instagram.com/khairahscorner"
+                  href="https://shecodeafrica.org/"
                 >
-                  <IGIcon />
+                  <WebIcon />
                 </a>
               </div>
               <div className="round-icon">
@@ -360,24 +247,6 @@ const Home = () => {
                   href="https://www.quora.com/profile/Airah-Yusuff"
                 >
                   <QuoraIcon />
-                </a>
-              </div>
-              <div className="round-icon">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://pinterest.com/khairahscorner"
-                >
-                  <PinterestIcon />
-                </a>
-              </div>
-              <div className="round-icon">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://www.gopicky.com/skincarebiased"
-                >
-                  <PickyIcon />
                 </a>
               </div>
             </div>
@@ -391,7 +260,7 @@ const Home = () => {
           <FooterLogo />
         </div>
         <div className="credit">
-          © {currentYear} - Airah Yusuff. All rights reserved.
+          © {currentYear} - Airat Yusuff. All rights reserved.
         </div>
       </FooterWrapper>
     </div>
